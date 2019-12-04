caf8d9bce4887d501322952b774f5fa486a33e31
simfeo <simidimus@gmail.com>
2019-12-04 21:45:04 +0200

    updated for public view

--- armenian_enhance.py
+++ armenian_enhance.py
@@ -1,20 +1,35 @@
+# ArmASCII8 to utf converter
+# specify input and output documents
+# tested on python 3.7
+import sys
+
 arm_map = {}
 
 for i in range(178,253,2):
     arm_map[i] = int(1328 + (i-176)/2)
     arm_map[i+1] = int(1376 + (i-176)/2)
 
-f = open('C:\media\document.xml','r', encoding="utf-8")
-lines = f.readlines()
-f.close()
+def main(infile = None, outfile = None):
+    if not infile:
+        infile = sys.argv[1]
+    
+    if not outfile:
+        outfile = sys.argv[2]
+        
+    f = open(infile,'r', encoding="utf-8")
+    lines = f.readlines()
+    f.close()
 
+    f = open(outfile, 'w', encoding='utf-8')
+    for line in lines:
+        for l in line:
+            if ord(l) in arm_map:
+                f.write(chr(arm_map[ord(l)]))
+            else:
+                f.write(l)
 
-f = open("out.xml", 'w', encoding='utf-8')
-for line in lines:
-    for l in line:
-        if ord(l) in arm_map:
-            f.write(chr(arm_map[ord(l)]))
-        else:
-            f.write(l)
-
-f.close()
+    f.close()
+    print ("job is done!")
+    
+if __name__ == "__main__":
+    main()
